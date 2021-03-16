17283c26cd3e672009e6961a33eb98c8a8ebc097
Igor Albanezi Conceição <iac@cesar.org.br>
2021-03-16 14:57:12 -0300

    Add project base

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.use(express.json());
+
+app.get('/healthcheck', (req, res) => {
+  res.json({ status: 'online' })
 });
 
 app.listen(port, () => {
